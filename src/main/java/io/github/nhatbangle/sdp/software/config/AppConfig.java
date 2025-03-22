7b66d9b39e492e2154d4105b3d6b38de8fa6a0d8
NhatBangLe <bangle37568@gmail.com>
2025-03-22 10:19:03 +0700

    Remove CORS and LoadBalancer

--- src/main/java/io/github/nhatbangle/sdp/software/config/AppConfig.java
+++ src/main/java/io/github/nhatbangle/sdp/software/config/AppConfig.java
@@ -1,31 +1,17 @@
 package io.github.nhatbangle.sdp.software.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
 @EnableAsync
 @EnableConfigurationProperties(AppConfigProps.class)
-public class AppConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .maxAge(3600);
-    }
+public class AppConfig {
 
     @Bean
-    @LoadBalanced
     public RestClient.Builder restClientBuilder() {
         return RestClient.builder();
     }
